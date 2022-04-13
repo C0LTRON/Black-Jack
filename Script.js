0400649c3eab92a45db05bf56129139849fb8222
C0LTRON <103008726+C0LTRON@users.noreply.github.com>
2022-04-13 16:03:11 -0700

    Debugged generateShoe() and shuffle(), both are working as intended

--- Script.js
+++ Script.js
@@ -2,7 +2,19 @@ const suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
 const name = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
 const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 
+
+
+//This creates the Shoe that the cards will be dealt from.
 let shoe = generateShoe();
+
+//Call this if debugging shuffle state of the shoe
+function debugShoe(shoe){
+    for(let i = 0; i < 25; i++){
+        let x = JSON.stringify (shoe[i]);
+        console.log(x);
+    }
+console.log("\n");
+}
 
 //This function creates all the cards we will play with
 //For clarity, a "shoe" is the name casino's use for multiple decks of cards shuffled together. 
@@ -12,7 +24,7 @@ function generateShoe() {
     let shoe = new Array();
     // this creates 8 decks of cards
     for(let e = 0; e < 8; e++){
-        //This creates one card of each suit, of which there are only 4
+        //This creates one of each card per suit, of which there are only 4
         for(let i = 0; i < suit.length; i++){
             //this creates a card of each name/value, of which there are 13
             for(let x = 0; x < value.length; x++){
@@ -22,9 +34,56 @@ function generateShoe() {
             }
         }
     }
+    shoe = shuffle(shoe);
+    return shoe;
+}   
+//Now that our shoe has been built, it needs to be shuffled.
+//This can be done in O(n) by utilizing the Fisher-Yates algorithm. This algorithm moves every card at least once to a random index within the shoe array. https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffle(shoe){  
+    let x = shoe.length, a, b; 
+        while (x){
+          
+          a = Math.floor((Math.random() * x--));
+          
+          b = shoe[x];
+          shoe[x] = shoe[a];
+          shoe[a] = b;
+        }
+    debugShoe(shoe);
     return shoe;
 }
-for(let i = 0; i < 416; i++){
-  let x = JSON.stringify (shoe[i]);
-  document.writeln(x)  
-}
+    
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
