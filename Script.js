c41b3422008614ce9e7c251d64078e64d70505af
C0LTRON <103008726+C0LTRON@users.noreply.github.com>
2022-04-12 22:46:46 -0700

    nothing significant

--- Script.js
+++ Script.js
@@ -4,13 +4,19 @@ const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 
 let shoe = generateShoe();
 
+//This function creates all the cards we will play with
+//For clarity, a "shoe" is the name casino's use for multiple decks of cards shuffled together. 
+//For this program, our "shoe" will consist of 8 decks shuffled together
 function generateShoe() {
-    
+    // declare empty shoe array to insert cards into
     let shoe = new Array();
+    // this creates 8 decks of cards
     for(let e = 0; e < 8; e++){
+        //This creates one card of each suit, of which there are only 4
         for(let i = 0; i < suit.length; i++){
+            //this creates a card of each name/value, of which there are 13
             for(let x = 0; x < value.length; x++){
-
+                // this pushes the new card created into the shoe
                 let card = {Value: value[x], Name: name[x], Suit: suit[i]};
                 shoe.push(card);
             }
